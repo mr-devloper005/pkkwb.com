95b27b3a2877e11ca2a9b2f34f1907225463f387
root <root@srv1489383.hstgr.cloud>
2026-05-22 08:53:57 +0000

    Use optimized GHCR pull deploy

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
